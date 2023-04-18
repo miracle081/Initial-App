89e8193efcc81950bff5e07ffc772a03f7237f80
Miracle Obafemi <100706618+miracle081@users.noreply.github.com>
2023-04-18 11:57:50 +0100

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { View } from "react-native";
 import { UI } from "./Components/UI.js";
 
 export default function app() {
+//   Testing github
   return (
     <View style={{flex:1}}>
       <UI />
